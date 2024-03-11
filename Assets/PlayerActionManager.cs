6ca6dbcff036553c936a74e436da05682b9090e7
Rakshaan Khan <55557308+rakshaankhan@users.noreply.github.com>
2024-03-11 18:41:51 -0400

    Basic
    Just the beginning stuff.

---
+++ Assets/PlayerActionManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActionManager : MonoBehaviour
+{
+
+
+
+    private void OnEnable()
+    {
+
+        RegisterActions();
+
+
+
+    }
+
+
+    private void OnDisable()
+    {
+
+
+
+    }
+
+    void RegisterActions();
+    {
+    
+
+
+    }
+
+
+
+}
+
+    
