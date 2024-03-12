984579f0d9ab9ab5ded7feaeb17d9b9e24e47687
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-03-11 17:27:12 -0700

    very basic jump

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 public class PlayerJump : MonoBehaviour
 {
     PlayerActionManager playerActionManager;
+    Rigidbody2D rb;
+
+    [SerializeField] public Transform groundCheck;
+    [SerializeField] public float groundCheckRadius = 1;
+    [SerializeField] public int groundCheckLayerMask;
     void Start()
     {
         playerActionManager = GetComponent<PlayerActionManager>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void Jump(float jumpHeight)
     {
-        if(playerActionManager.jumpValue)
+        if(playerActionManager.jumpValue && IsGrounded())
         {
-            Debug.Log("Jump");
+            //Debug.Log("jump");
+            rb.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
         }
         
     }
 
     bool IsGrounded()
     {
+        if(Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundCheckLayerMask) == null) { return false; }
         return true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
