766f3646fee5c0f7a3da2ce9ab33adc6c24e94fc
Rakshaan Khan <55557308+rakshaankhan@users.noreply.github.com>
2024-03-14 20:29:23 -0400

    Start, Restart on screen, Main menu and Logo all fit to add into!

---
+++ Assets/Scripts/Raksh/Restart.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+    }
+
+}
